b9cee7a9194714a342c95d52c3d037c1f3e13180
João Dinis Ferreira <hello@joaof.eu>
2026-07-11 16:33:33 +0200

    probe: final-chain scoped timing (temporary)

--- com.avaloq.tools.ddk.xtext.format/src/com/avaloq/tools/ddk/xtext/format/FormatRuntimeModule.java
+++ com.avaloq.tools.ddk.xtext.format/src/com/avaloq/tools/ddk/xtext/format/FormatRuntimeModule.java
@@ -123,3 +123,5 @@ public class FormatRuntimeModule extends AbstractFormatRuntimeModule {
     binder.bind(IXtextProjectConfig.class).to(XtextProjectConfig.class);
   }
 }
+
+// final-chain scoped probe (temporary)
